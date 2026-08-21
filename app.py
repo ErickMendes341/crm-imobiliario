5ab1ea23c8d482778b5196542dfc1475b11a5178
ErickMendes341 <erickmendes341@gmail.com>
2026-08-21 13:00:59 -0300

    Change SUPABASE_KEY to a new value
    Updated SUPABASE_KEY to use a new environment variable value.

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ BAIRROS_PASSOS = sorted([
 
 # --- CONEXÃO SUPABASE ---
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://dsnamhmffvjxcfqtlzet.supabase.co")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "sb_publishable_XVO9PLxpxWBnr32_UYt_UA_HSdspi16")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbmFtaG1mZnZqeGNmcXRsemV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzYwOTYsImV4cCI6MjEwMTcxMjA5Nn0.e9Uqxp0qv_ifezQ29q-7qcAKmBmzo7-wD5GwK-Bxqts")
 
 @st.cache_resource
 def init_supabase():
